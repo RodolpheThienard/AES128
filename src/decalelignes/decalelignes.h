104d7c9fc50449b9e1ee85c4571970007c2d398c
RodolpheThienard <thienard.rodolphe1@gmail.Com>
2022-03-13 10:46:26 +0100

    ifndef for suboctet & decaleligne

--- src/decalelignes/decalelignes.h
+++ src/decalelignes/decalelignes.h
@@ -1,6 +1,11 @@
+#ifndef DECALELIGNE_H
+#define DECALELIGNE_H
+
 #include <stdio.h>
 #include <stdlib.h>
 
 void free_matrix(int x,int** tab);
 void DecaleLignes(Block* a, int nb_block);
 void display_matrix(int x, int y, int** tab);
+
+#endif
