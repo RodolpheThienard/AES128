f947a739195ab39fba979c5dd2fb963cf0ee88e4
lilifus <zakaria.ejjed@gmail.com>
2022-03-13 13:52:53 +0100

    retrait fonction superflue decaleligne

--- src/decalelignes/decalelignes.c
+++ src/decalelignes/decalelignes.c
@@ -1,33 +1,6 @@
 #include "../suboctet/suboctet.h"
 #include "decalelignes.h"
 
-void free_matrix(int x,int** tab){
-    for ( int i = 0; i<x; i++ )
-    {
-        free(tab[i]);
-    }
-    free(tab);
-}
-
-//version pour une matrice seul
-// int** DecaleLignes(int** a){
-//     //on créait une nouvelle matrice pour pouvoir décaler les valeurs sans problemes
-//     int ** b = malloc( 4*sizeof(int*) );
-//     for ( int i = 0; i<4; i++ ){
-//         b[i] = malloc( 4*sizeof(int) );
-//     }
-
-//     //attribution des valeurs avec le décalage
-//     for(int i=0;i<4;i++){
-//         for(int j=0;j<4;j++){
-//             b[i][j]=a[i][(j+i)%4]; // (i+j)%4 permet d'avoir exactement le décalage demandé
-//         }
-//     }
-//     free_matrix(4,a);//on free l'ancienne matrice
-//     return b;//on return la nouvelle matrice qui a beneficié du décalage
-// }
-
-//VERSION POUR FONCTIONNER AVEC LA STRUCT BLOCK
 void DecaleLignes(Block* a, int nb_block){
     int value = 0;
     //On fait toutes les matrices
@@ -52,13 +25,3 @@ void DecaleLignes(Block* a, int nb_block){
         }
     }    
 }
-
-void display_matrix(int x, int y, int** tab){
-    for(int i = 0;i<x;i++){
-        for(int j = 0; j<y;j++){
-            printf("|%2x|",tab[i][j]);
-        }
-        printf("\n");
-    }
-}
-
