6e9552b14cddcf0f952e4a5fdd256a1a0e0dd516
Ronan <23docherty.ronan@gmail.com>
2022-05-08 22:13:39 +0200

    ajout de commentaire sur aes et melangecolonnes

--- src/aes/aes.c
+++ src/aes/aes.c
@@ -6,7 +6,7 @@
 #include "../tools/tools.h"
 #include "../ctr/ctr.h"
 
-
+//Effectue un tour d'AES sur une matrice avec une clé précédemment crée
 void turn(int **matrix, int **key)
 {
      suboctet(matrix);
@@ -15,6 +15,7 @@ void turn(int **matrix, int **key)
      addroundkey(matrix, key);
 }
 
+//Effectue le dernier tour d'AES sans mélanger les colonnes
 void last_turn(int **matrix, int **key)
 {
      suboctet(matrix);
@@ -22,6 +23,7 @@ void last_turn(int **matrix, int **key)
      addroundkey(matrix, key);
 }
 
+//Effectue autant de tours d'AES que ceux mis en argument et se termine par un tour sans mélanger les colonnes
 void aes(int **matrix, int **extended_key, int turns)
 {
      addroundkey(matrix, extended_key);
